68c25dd31928d2b36de39d6ed2330d926fd0ffdb
林 永遠 <hayasi@naniwacc.co.jp>
2022-03-30 15:38:04 +0900

    appended mklink for expand.bat

--- mklink.bat
+++ mklink.bat
@@ -1,2 +1,3 @@
 mklink /h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\my.ahk" "my.ahk"
 mklink /h "%HOME%\AppData\Local\nvim\init.vim" "init.vim"
+mklink /h "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" "expand.bat"
