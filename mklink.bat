8a5a2431eabebf659e3e3168ecb1fd4b6e5f2bf6
staticWagomU <to6wa17@gmail.com>
2022-03-31 13:13:59 +0900

    Added command to remove old link

--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,8 @@
-mklink /h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\my.ahk" "my.ahk"
-mklink /h "%HOME%\AppData\Local\nvim\init.vim" "init.vim"
-mklink /h "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" "expand.bat"
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\my.ahk"
+mklink /h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\my.ahk" ".\my.ahk"
+
+del "%HOME%\AppData\Local\nvim\init.vim" 
+mklink /h "%HOME%\AppData\Local\nvim\init.vim" ".\init.vim"
+
+del "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" 
+mklink /h "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" ".\expand.bat"
