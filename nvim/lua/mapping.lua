4cd01bbb7431ef1c02ce2542fa10a9d148a510d6
staticWagomU <to6wa17@gmail.com>
2022-08-23 17:10:51 +0900

    fix mappings

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -10,15 +10,15 @@ m("c", "<C-x>", "<C-r>=expand('%:p')<CR>", {})
 m("c", "<C-z>", "<C-r>=expand('%:p:r')<CR>", {})
 
 m("n", "<Leader>ls", ":<C-u>ls<CR>", { noremap = true })
-m("n", "<Leader>w", ":<C-u>w<CR>", { noremap = true }
-m("n", "<Leader>bn", ":<C-u>bn<CR>", { noremap = true }
-m("n", "<Leader>bp", ":<C-u>bp<CR>", { noremap = true }
-m("n", "<Leader>bd", ":<C-u>bd<CR>", { noremap = true }
+m("n", "<Leader>w", ":<C-u>w<CR>", { noremap = true })
+m("n", "<Leader>bn", ":<C-u>bn<CR>", { noremap = true })
+m("n", "<Leader>bp", ":<C-u>bp<CR>", { noremap = true })
+m("n", "<Leader>bd", ":<C-u>bd<CR>", { noremap = true })
 m("n", "<Leader>cd", ":<C-u>cd %:p:h<CR>", silent)
 
-m("t", "<ESC>", "<C-\><C-n>", silent)
-m("t", "<C-w><C-l>", "<C-\><C-n><C-w><C-l>", silent)
-m("t", "<C-w><C-h>", "<C-\><C-n><C-w><C-h>", silent)
+m("t", "<ESC>", "<C-\\><C-n>", silent)
+m("t", "<C-w><C-l>", "<C-\\><C-n><C-w><C-l>", silent)
+m("t", "<C-w><C-h>", "<C-\\><C-n><C-w><C-h>", silent)
 
 m("n", ";", "<Nop>", silent)
 m("n", "[lsp]", "<Nop>", silent)
