ad24b01bc78932755e3f628c583ea128dfa77d9a
staticWagomU <to6wa17@gmail.com>
2022-08-23 23:37:38 +0900

    change

--- nvim/lua/pluginconfig/lspsaga.lua
+++ nvim/lua/pluginconfig/lspsaga.lua
@@ -83,26 +83,26 @@ lspsaga.init_lsp_saga({ -- defaults ...
 	},
 })
 
--- vim.keymap.set("n", "[lsp]r", "<cmd>Lspsaga rename<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "M", "<cmd>Lspsaga code_action<cr>", { silent = true, noremap = true })
--- vim.keymap.set("x", "M", ":<c-u>Lspsaga range_code_action<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "?", "<cmd>Lspsaga hover_doc<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]o", "<cmd>Lspsaga show_line_diagnostics<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]j", "<cmd>Lspsaga diagnostic_jump_next<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]k", "<cmd>Lspsaga diagnostic_jump_prev<cr>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]f", "<cmd>Lspsaga lsp_finder<CR>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]s", "<cmd>Lspsaga signature_help<CR>", { silent = true, noremap = true })
--- vim.keymap.set("n", "[lsp]d", "<cmd>Lspsaga preview_definition<CR>", { silent = true })
--- vim.keymap.set("n", "[lsp]o", "<cmd>LSoutlineToggle<CR>", { silent = true })
--- vim.keymap.set(
--- 	"n",
--- 	"<C-b>",
--- 	"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
--- 	{ silent = true, noremap = true }
--- )
--- vim.keymap.set(
--- 	"n",
--- 	"<C-f>",
--- 	"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
--- 	{ silent = true, noremap = true }
--- )
+ vim.keymap.set("n", "[lsp]r", "<cmd>Lspsaga rename<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "M", "<cmd>Lspsaga code_action<cr>", { silent = true, noremap = true })
+ vim.keymap.set("x", "M", ":<c-u>Lspsaga range_code_action<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "?", "<cmd>Lspsaga hover_doc<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]o", "<cmd>Lspsaga show_line_diagnostics<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]j", "<cmd>Lspsaga diagnostic_jump_next<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]k", "<cmd>Lspsaga diagnostic_jump_prev<cr>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]f", "<cmd>Lspsaga lsp_finder<CR>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]s", "<cmd>Lspsaga signature_help<CR>", { silent = true, noremap = true })
+ vim.keymap.set("n", "[lsp]d", "<cmd>Lspsaga preview_definition<CR>", { silent = true })
+ vim.keymap.set("n", "[lsp]o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+ vim.keymap.set(
+ 	"n",
+ 	"<C-b>",
+ 	"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
+ 	{ silent = true, noremap = true }
+ )
+ vim.keymap.set(
+ 	"n",
+ 	"<C-f>",
+ 	"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
+ 	{ silent = true, noremap = true }
+ )
--- nvim/lua/pluginconfig/mason.lua
+++ nvim/lua/pluginconfig/mason.lua
@@ -10,15 +10,53 @@ mason.setup({
 })
 
 local nvim_lsp = require('lspconfig')
-local mason_lspconfig = require('mason-lspconfig')
-mason_lspconfig.setup_handlers({ function(server_name)
-        local opts = {}
-        opts.on_attach = function(_, bufnr)
-                local bufopts = { silent = true, buffer = bufnr }
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                vim.keymap.set('n', 'gtD', vim.lsp.buf.type_definition, bufopts)
-                vim.keymap.set('n', 'grf', vim.lsp.buf.references, bufopts)
-                vim.keymap.set('n', '<space>p', vim.lsp.buf.format, bufopts)
-        end
-        nvim_lsp[server_name].setup(opts)
-end })
+require("mason-lspconfig").setup()
+local on_attach = function(client, bufnr)
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
+
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	local opts = { noremap = true, silent = true }
+
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "?", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "g?", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "[lsp]wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "[lsp]wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "[lsp]wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "[lsp]D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	-- buf_set_keymap("n", "[lsp]rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "[lsp]a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "[lsp]e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "[lsp]q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	buf_set_keymap("n", "[lsp]f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+	-- require("lsp_signature").on_attach()
+	-- require("illuminate").on_attach(client)
+	require("nvim-navic").attach(client, bufnr)
+end
+
+local lspconfig = require("lspconfig")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local opts = { capabilities = capabilities, on_attach = on_attach }
+
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		lspconfig[server_name].setup(opts)
+	end,
+--	["zls"] = function()
+--	end,
+})
