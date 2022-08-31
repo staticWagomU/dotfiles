b1302a6eb97add714fd6356f13b88b15554d2ed3
staticWagomU <to6wa17@gmail.com>
2022-08-31 15:25:42 +0900

    change

--- nvim/lua/pluginconfig/alpha-nvim.lua
+++ nvim/lua/pluginconfig/alpha-nvim.lua
@@ -15,9 +15,10 @@ dashboard.section.header.val = banner
 --dashboard.section.footer.val = vim.fn['s:print_plugins_message']()
 
 dashboard.section.buttons.val = {
-	dashboard.button("e", " New file", ":enew<CR>"),
+	dashboard.button("n", " New file", ":enew<CR>"),
 	dashboard.button("t", " Telescope", ":Telescope<CR>"),
 	dashboard.button("f", " Find file", ":Telescope find_files<CR>"),
+	dashboard.button("e", " File browser", ":Telescope file_browser<CR>"),
 	dashboard.button("i", " Init.vim", ":cd ~/dotfiles/nvim/lua<CR>:Telescope file_browser<CR>"),
 	dashboard.button("u", " Update plugins", ":PackerSync<CR>"),
 	dashboard.button("q", " Exit", ":qa<CR>"),
