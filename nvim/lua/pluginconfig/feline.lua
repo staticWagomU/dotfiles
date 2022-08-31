9083a0ea26cbe8ea91efe87dcd312cf09fe219e7
staticWagomU <to6wa17@gmail.com>
2022-08-31 11:17:05 +0900

    felineの設定を追加

--- nvim/lua/pluginconfig/feline.lua
+++ nvim/lua/pluginconfig/feline.lua
@@ -1,2 +1,18 @@
 require('feline').setup()
 
+-- local navic = require("nvim-navic")
+--
+-- local components = {
+--         active = {{}, {}, {}},
+-- }
+--
+-- table.insert(components.active[1], {
+--         provider = function()
+--                 return navic.get_location()
+--         end,
+--         enabled = function() return navic.is_available() end,
+-- })
+--
+-- require("feline").setup({
+--         components = components
+-- })
