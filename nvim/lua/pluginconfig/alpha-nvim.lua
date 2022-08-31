015442df56b2165f9d4b0ee344a6149bba7fd07e
staticWagomU <to6wa17@gmail.com>
2022-08-31 09:56:45 +0900

    絵文字削除

--- nvim/lua/pluginconfig/alpha-nvim.lua
+++ nvim/lua/pluginconfig/alpha-nvim.lua
@@ -15,13 +15,12 @@ dashboard.section.header.val = banner
 --dashboard.section.footer.val = vim.fn['s:print_plugins_message']()
 
 dashboard.section.buttons.val = {
-	dashboard.button("e", "📓 New file", ":enew<CR>"),
-	dashboard.button("t", "🔭 Telescope", ":Telescope<CR>"),
-	dashboard.button("f", "🔎 Find file", ":Telescope find_files<CR>"),
-	dashboard.button("i", "🔧 Init.vim", ":cd ~/.dotfiles/nvim/lua<CR>:Telescope file_browser<CR>"),
-	-- dashboard.button("g", "🐻 Ginit.vim", ":e ~/.dotfiles/nvim/rc/ginit.vim<CR>"),
-	dashboard.button("u", "📫 Update plugins", ":PackerSync<CR>"),
-	dashboard.button("q", "🚪 Exit", ":qa<CR>"),
+	dashboard.button("e", " New file", ":enew<CR>"),
+	dashboard.button("t", " Telescope", ":Telescope<CR>"),
+	dashboard.button("f", " Find file", ":Telescope find_files<CR>"),
+	dashboard.button("i", " Init.vim", ":cd ~/.dotfiles/nvim/lua<CR>:Telescope file_browser<CR>"),
+	dashboard.button("u", " Update plugins", ":PackerSync<CR>"),
+	dashboard.button("q", " Exit", ":qa<CR>"),
 }
 
 require'alpha'.setup(dashboard.config)
