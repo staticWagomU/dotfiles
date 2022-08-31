8c2a361b9dd4ddc175bc5b71de181e693cf929a1
staticWagomU <to6wa17@gmail.com>
2022-08-31 14:47:27 +0900

    os別対応

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,12 @@
 local wezterm = require("wezterm")
+local dprog
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+        dprog = {os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe"}
+else
+        dprog = {}
+end
 return {
-<<<<<<< HEAD
-        if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-                default_prog = {os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe")},
-        end
-=======
-        default_prog = {os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe"},
->>>>>>> 711e6a9a355c1b2515e50d825a1907f028a0e5b6
-        color_scheme = "Dracula",
+        default_prog = dprog,
         font_size = 10.0,
         font = wezterm.font "JetBrains Mono",
         harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
