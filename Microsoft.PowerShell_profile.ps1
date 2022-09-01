516add2faf0736fec79c825c7af13c5de3410e59
staticWagomU <to6wa17@gmail.com>
2022-09-01 14:31:26 +0900

    エイリアス追加 csl->Clear-Host

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,12 +2,10 @@ if(-not $env:path.Split(';').Contains('.')){
     $env:path += ";."
 }
 
-#if ($env:WT_PROFILE_ID) {
-        oh-my-posh init pwsh | Invoke-Expression
-        Import-Module oh-my-posh
-                Set-PoshPrompt -Theme avit
-#Set-PoshPrompt -Theme nordtron
-#New-Item (Split-Path -Parent $PROFILE) -type directory -Force
-#}
+oh-my-posh init pwsh | Invoke-Expression
+Import-Module oh-my-posh
+        Set-PoshPrompt -Theme avit
+
+Set-Alias csl Clear-Host
 
 
