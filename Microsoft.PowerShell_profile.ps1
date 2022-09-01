a663d06af1d04934aa2bdb12f8757fcb2a211faf
staticWagomU <to6wa17@gmail.com>
2022-09-01 16:29:05 +0900

    alias追加

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,10 @@ oh-my-posh init pwsh | Invoke-Expression
 Import-Module oh-my-posh
         Set-PoshPrompt -Theme avit
 
-Set-Alias csl Clear-Host
+function git_status() {git status}
 
+Set-Alias csl Clear-Host
+Set-Alias n nvim
+Set-Alias v vim
+Set-Alias gs git_status
 
