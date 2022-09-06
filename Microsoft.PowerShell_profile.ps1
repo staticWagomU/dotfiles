a79fa5502b2e93672de2b5af2e64ab3eb00109c7
staticWagomU <to6wa17@gmail.com>
2022-09-06 13:43:53 +0900

    mkdir&cdするコマンド追加

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,35 @@ if(-not $env:path.Split(';').Contains('.')){
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/avit.omp.json" | Invoke-Expression
 function git_status() {git status}
 
+function mkdir_cd {
+        [CmdletBinding()]
+                param(
+                                [Parameter(Mandatory = $true)]
+                                $Path
+                     )
+
+                        New-Item -Path $Path -ItemType Directory
+
+                        Set-Location -Path $Path
+}
+
+function mkdir_cd_cls {
+        [CmdletBinding()]
+                param(
+                                [Parameter(Mandatory = $true)]
+                                $Path
+                     )
+
+                        New-Item -Path $Path -ItemType Directory
+
+                        Set-Location -Path $Path
+                        Clear-Host
+}
+
 Set-Alias csl Clear-Host
 Set-Alias cl Clear-Host
 Set-Alias n nvim
 Set-Alias v vim
 Set-Alias gs git_status
-
+Set-Alias mcd mkdir_cd
+Set-Alias mccd mkdir_cd_cls
