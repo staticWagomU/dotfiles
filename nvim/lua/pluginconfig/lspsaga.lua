244584c800c8bf507463b887fc001649687f4074
staticWagomU <to6wa17@gmail.com>
2022-09-12 21:29:29 +0900

    lspsagaの設定変更

--- nvim/lua/pluginconfig/lspsaga.lua
+++ nvim/lua/pluginconfig/lspsaga.lua
@@ -21,9 +21,9 @@ lspsaga.init_lsp_saga({ -- defaults ...
   -- diagnostic_header = { " ", " ", " ", "ﴞ " },
   diagnostic_header = { "😡", "😥", "😤", "😐" },
   -- show diagnostic source
-  show_diagnostic_source = true,
+  -- show_diagnostic_source = true,
   -- add bracket or something with diagnostic source, just have 2 elements
-  diagnostic_source_bracket = {},
+  -- diagnostic_source_bracket = {},
   -- preview lines of lsp_finder and definition preview
   max_preview_lines = 10,
   -- use emoji lightbulb in default
@@ -60,11 +60,11 @@ lspsaga.init_lsp_saga({ -- defaults ...
     exec = "<CR>",
   },
   rename_action_quit = "<C-c>",
-  definition_preview_icon = "  ",
+  -- definition_preview_icon = "  ",
   -- show symbols in winbar must nightly
   symbol_in_winbar = {
     in_custom = false,
-    enable = false,
+    enable = true,
     separator = " ",
     show_file = true,
     click_support = false,
@@ -107,3 +107,55 @@ vim.keymap.set(
   "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>",
   { silent = true, noremap = true }
 )
+
+local keymap = vim.keymap.set
+
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+
+-- Code action
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+
+-- Rename
+keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
+
+-- Peek Definition
+-- you can edit the definition file in this flaotwindow
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+
+-- Show line diagnostics
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+
+-- Show cursor diagnostic
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+
+-- Diagnsotic jump can use `<c-o>` to jump back
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+
+-- Only jump to error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+
+-- Outline
+keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+
+-- Hover Doc
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+
+-- Float terminal
+keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm .<CR>", { silent = true })
+-- if you want pass somc cli command into terminal you can do like this
+-- open lazygit in lspsaga float terminal
+keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm lazygit<CR>", { silent = true })
+-- close floaterm
+keymap("t", "<A-d>", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
