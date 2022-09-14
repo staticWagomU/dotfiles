7e5b5ad13257d0b73b73df4483535f036f95a50b
staticWagomU <to6wa17@gmail.com>
2022-09-14 13:05:35 +0900

    change

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require("wezterm")
 local dprog
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-        dprog = {os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe"}
+  dprog = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
 else
-        dprog = {}
+  dprog = { "zsh" }
 end
 return {
-        default_prog = dprog,
-        font_size = 10.0,
-        font = wezterm.font "JetBrains Mono",
-        harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-        color_scheme = "iceberg-dark"
+  default_prog = dprog,
+  font_size = 10.0,
+  font = wezterm.font "JetBrains Mono",
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  color_scheme = "iceberg-dark"
 }
