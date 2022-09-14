dc04d6b0762e39a1b05c2774a22d6dd884f5bb7e
staticWagomU <to6wa17@gmail.com>
2022-09-14 13:49:29 +0900

    lua_help()関数追加

--- nvim/lua/pluginconfig/lspsaga.lua
+++ nvim/lua/pluginconfig/lspsaga.lua
@@ -38,33 +38,13 @@ end
 local keymap = vim.keymap.set
 
 lspsaga.init_lsp_saga({ -- defaults ...
-  -- Options with default value
   -- "single" | "double" | "rounded" | "bold" | "plus"
   border_style = "single",
-  --the range of 0 for fully opaque window (disabled) to 100 for fully
-  --transparent background. Values between 0-30 are typically most useful.
   saga_winblend = 0,
-  -- when cursor in saga window you config these to move
   move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-  -- Error, Warn, Info, Hint
-  -- use emoji like
-  -- { "🙀", "😿", "😾", "😺" }
-  -- or
-  -- { "😡", "😥", "😤", "😐" }
-  -- and diagnostic_header can be a function type
-  -- must return a string and when diagnostic_header
-  -- is function type it will have a param `entry`
-  -- entry is a table type has these filed
-  -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
   -- diagnostic_header = { " ", " ", " ", "ﴞ " },
   diagnostic_header = { "😡", "😥", "😤", "😐" },
-  -- show diagnostic source
-  -- show_diagnostic_source = true,
-  -- add bracket or something with diagnostic source, just have 2 elements
-  -- diagnostic_source_bracket = {},
-  -- preview lines of lsp_finder and definition preview
   max_preview_lines = 10,
-  -- use emoji lightbulb in default
   code_action_icon = " ",
   -- if true can press number to execute the codeaction in codeaction window
   code_action_num_shortcut = true,
