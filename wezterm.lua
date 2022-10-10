922806c6b97c8947661abb9eee4e6e27f28864d8
staticWagomU <to6wa17@gmail.com>
2022-10-10 21:31:31 +0900

    macでも動作するように修正

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,13 @@
 local wezterm = require("wezterm")
-local dprog
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  dprog = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
-else
-  dprog = { "zsh" }
-end
-return {
+local opts = {
   default_prog = dprog,
   font_size = 10.0,
   font = wezterm.font "JetBrains Mono",
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   color_scheme = "iceberg-dark"
 }
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  opts["dprog"] = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
+end
+
+return opts
