0008aa7874468689c96a1d34ed8e7c1e3ad95211
staticWagomU <to6wa17@gmail.com>
2022-10-11 13:20:33 +0900

    flake8+black追加

--- nvim/lua/pluginconfig/null-ls.lua
+++ nvim/lua/pluginconfig/null-ls.lua
@@ -1,2 +1,15 @@
-require("null-ls").setup({
+local null_ls = require("null-ls")
+
+-- sources setup
+local sources = {
+  -- JavaScript
+  null_ls.builtins.formatting.prettier,
+  -- Python
+  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.formatting.black
+}
+null_ls.setup({
+  -- debug = true,
+  sources = sources,
+  on_attach = require("plugins.configs.nvim_lsp.nvim_lsp").on_attach,
 })
