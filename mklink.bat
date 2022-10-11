f9399ceb7d399fa7f5619a1e92f8b744221a0e54
staticWagomU <to6wa17@gmail.com>
2022-10-11 13:20:00 +0900

    vimのmklink追加

--- mklink.bat
+++ mklink.bat
@@ -3,3 +3,9 @@ mklink /h "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\my.ahk" 
 
 del       "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" 
 mklink /h "%HOME%\AppData\Roaming\Microsoft\Windows\SendTo\expand.bat" ".\expand.bat"
+
+del "%VIM%/_vimrc"
+del "%HOME%\_vimrc"
+mklink /h "%HOME%\_vimrc" "%HOME%\dotfiles\vim\rc\_vimrc"
+del "%HOME%\_gvimrc"
+mklink /h "%HOME%\_gvimrc" "%HOME%\dotfiles\vim\rc\gvimrc"
