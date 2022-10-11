b9077c44dbc43bdd91510995a8a1554f394722ac
staticWagomU <to6wa17@gmail.com>
2022-10-11 17:43:24 +0900

    読み込まないプラグインの変数をoptions.luaへ移動

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,9 +1,24 @@
+local vim                      = vim
+vim.g.loaded_gzip              = 1
+vim.g.loaded_tar               = 1
+vim.g.loaded_tarPlugin         = 1
+vim.g.loaded_zip               = 1
+vim.g.loaded_zipPlugin         = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_vimball           = 1
+vim.g.loaded_vimballPlugin     = 1
+vim.g.loaded_getscript         = 1
+vim.g.loaded_getscriptPlugin   = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_netrwFileHandlers = 1
 vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.g.mapleader = " "
-
 
 local set = vim.opt
 
