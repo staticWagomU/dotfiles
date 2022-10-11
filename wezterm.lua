1cf3c80e359526c00c288523d76e3315debe7cfb
staticWagomU <to6wa17@gmail.com>
2022-10-11 10:36:59 +0900

    change

--- wezterm.lua
+++ wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require("wezterm")
+local pwsh_path =  os.getenv("LOCALAPPDATA") .. [[\Microsoft\WindowsApps\Microsoft.PowerShell_8wekyb3d8bbwe\pwsh.exe]]
 local opts = {
-  default_prog = dprog,
   font_size = 10.0,
   font = wezterm.font "JetBrains Mono",
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   color_scheme = "iceberg-dark"
 }
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  opts["dprog"] = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" }
+  opts["default_prog"] = { pwsh_path }
 end
 
 return opts
