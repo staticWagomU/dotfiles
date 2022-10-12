5c8941cf0377b83014869421acf476134c455c71
staticWagomU <to6wa17@gmail.com>
2022-10-12 13:57:54 +0900

    change

--- nvim/lua/pluginconfig/luasnip.lua
+++ nvim/lua/pluginconfig/luasnip.lua
@@ -2,10 +2,20 @@
 vim.cmd([[imap <silent><expr> <C-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-k>']])
 vim.cmd([[smap <silent><expr> <C-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-k>']])
 local ls = require 'luasnip'
-local s = ls.s
-local sn = ls.sn
-local t = ls.t
-local i = ls.i
-local f = ls.f
-local c = ls.c
-local d = ls.d
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require 'luasnip.util.events'
+local ai = require 'luasnip.nodes.absolute_indexer'
+
+ls.snippets = {
+  javascript = {
+    s({ trig = "print" }, { t("console.log(") }, i(0), t({ ")" }))
+  }
+}
