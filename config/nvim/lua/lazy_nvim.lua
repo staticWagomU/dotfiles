d7c53fa8fd49bf62d75923c7db9761160de6667e
unknown <to6wa17@mail.com>
2023-04-19 13:34:31 +0900

    初期設定が間違えてた

--- config/nvim/lua/lazy_nvim.lua
+++ config/nvim/lua/lazy_nvim.lua
@@ -7,12 +7,12 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "--single-branch",
-    "git@github.com:folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins",{
   -- defaults = { lazy = true },
