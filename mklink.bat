06ea941aafdc7648c2e35d1092d601a089bef805
unknown <to6wa17@mail.com>
2023-04-19 13:33:54 +0900

    nvim用のmklink追加

--- mklink.bat
+++ mklink.bat
@@ -4,3 +4,5 @@ for /F %%A in ('dir .\config\vim /B') do (
   mklink /H %USERPROFILE%\vimfiles\%%A %USERPROFILE%\dotfiles\config\vim\%%A
 )
 endlocal
+
+mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotfiles\config\nvim
