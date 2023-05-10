112966a9ae3a5ba75c17d605d7403fdd3324e555
staticWagomU <to6wa17@gmail.com>
2023-05-10 15:31:32 +0900

    update: なんかうまく動かないので遅延消すよ

--- config/nvim/lua/plugins/fuzzymotion.lua
+++ config/nvim/lua/plugins/fuzzymotion.lua
@@ -4,9 +4,6 @@ return {
     "vim-denops/denops.vim",
     "lambdalisue/kensaku.vim",
   },
-  keys = {
-    { "<Leader><Leader>", ":<C-u>FuzzyMotion<CR>", desc="FuzzyMotion" },
-  },
   config = function()
     vim.keymap.set("n", "<Leader><Leader>", ":<C-u>FuzzyMotion<CR>", { noremap = true })
     vim.cmd [[
