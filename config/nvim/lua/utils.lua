02f1a08f219f6c21093ee82af5ee83e3c12b1c7e
staticWagomU <to6wa17@gmail.com>
2023-05-24 23:38:11 +0900

    tableの中身を見たくなるかもしれないから関数作成 忘れたころに役に立つと信じています

---
+++ config/nvim/lua/utils.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.dumpTable(t)
+   if type(t) == 'table' then
+      local s = '{ '
+      for k,v in pairs(t) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. M.dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(t)
+   end
+end
+
+return M
