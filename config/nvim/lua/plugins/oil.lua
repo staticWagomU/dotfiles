e6fad5f75961800d64184345569fda99724e835d
staticWagomU <to6wa17@gmail.com>
2023-05-29 16:04:03 +0900

    <leader>e のOil起動後にFuzzyMotionを実行する

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -5,7 +5,14 @@ return {
   config = function()
     require("oil").setup({
     })
-    vim.keymap.set("n", "<leader>e", "<cmd>Oil .<cr>", { noremap=true, silent=true })
+    vim.keymap.set("n", "<leader>e",
+    function ()
+      vim.cmd"Oil ."
+      vim.defer_fn(function()
+        vim.cmd.FuzzyMotion()
+      end, 300)
+    end,
+    { noremap=true, silent=true })
     vim.keymap.set("n", "<leader>E", "<cmd>Oil %:p:h<cr>", { noremap=true, silent=true })
   end,
   event = { "BufReadPre", "BufNewFile" },
