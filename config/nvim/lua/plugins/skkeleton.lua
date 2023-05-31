cc5c3244c1a7cca3486c1729bf8de654dc740fc6
staticWagomU <to6wa17@gmail.com>
2023-05-31 21:33:25 +0900

    autocmd内に閉じこめることに成功した

--- config/nvim/lua/plugins/skkeleton.lua
+++ config/nvim/lua/plugins/skkeleton.lua
@@ -2,21 +2,26 @@ return {
   {
   "vim-skk/skkeleton",
   config = function(p)
-    local dictdir = vim.fs.joinpath(vim.fs.dirname(p.dir), "dict")
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "DenopsPluginPost:skkeleton",
+      callback = function()
+        local dictdir = vim.fs.joinpath(vim.fs.dirname(p.dir), "dict")
 
-    vim.fn["skkeleton#config"]({
-      eggLikeNewline = true,
-      globalDictionaries = {
-        "~/skk/SKK-JISYO.L",
-        {vim.fs.joinpath(dictdir, "SKK-JISYO.L"), "utf-8" },
-        {vim.fs.joinpath(dictdir, "SKK-JISYO.hukugougo"), "utf-8" },
-        {vim.fs.joinpath(dictdir, "SKK-JISYO.mazegaki"), "utf-8" },
-        {vim.fs.joinpath(dictdir, "SKK-JISYO.propernoun"), "utf-8" },
-        {vim.fs.joinpath(dictdir, "SKK-JISYO.station"), "utf-8" },
-      },
-    })
-    vim.fn["skkeleton#register_kanatable"]("rom", {
-        [ [[z\<Space>]] ] = {[[\u3000]], ''},
+        vim.fn["skkeleton#config"]({
+          eggLikeNewline = true,
+          globalDictionaries = {
+            "~/skk/SKK-JISYO.L",
+            {vim.fs.joinpath(dictdir, "SKK-JISYO.L"), "utf-8" },
+            {vim.fs.joinpath(dictdir, "SKK-JISYO.hukugougo"), "utf-8" },
+            {vim.fs.joinpath(dictdir, "SKK-JISYO.mazegaki"), "utf-8" },
+            {vim.fs.joinpath(dictdir, "SKK-JISYO.propernoun"), "utf-8" },
+            {vim.fs.joinpath(dictdir, "SKK-JISYO.station"), "utf-8" },
+          },
+        })
+        vim.fn["skkeleton#register_kanatable"]("rom", {
+            [ [[z\<Space>]] ] = {[[\u3000]], ''},
+        })
+      end
     })
     vim.keymap.set({ "i", "c" }, "<C-j>", "<Plug>(skkeleton-toggle)")
   end
