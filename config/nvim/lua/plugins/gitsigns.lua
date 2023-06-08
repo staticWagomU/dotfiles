6b9da928fb7befb3f81f39d1dc95d0e6bd7bf1a9
staticWagomU <to6wa17@gmail.com>
2023-06-08 17:53:56 +0900

    私たち、別れきれてなかったみたい。。。をした

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -11,21 +11,4 @@ return {
       }
     end
   },
-  {
-    "lambdalisue/gin.vim",
-    dependencies = { "vim-denops/denops.vim" },
-    config = function ()
-      require("utils").make_abbrev({
-        { from = "git", to = "Gin" },
-        { from = "gin", to = "Gin" },
-        { from = "gs", to = "GinStatus" },
-        { from = "gp", to = "Gin push" },
-        { from = "gsh", to = "Gin show" },
-        { from = "gl", to = "GinLog --graph" },
-        { from = "glo", to = "GinLog --graph --oneline" },
-        { from = "gc", to = "Gin commit" },
-        { prepose = "Gin commit", from = "a", to = "--amend" },
-      })
-    end,
-  },
 }
