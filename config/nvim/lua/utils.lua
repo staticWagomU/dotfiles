36387cc12222a7a75eac6684d26d6f764dafc8cf
staticWagomU <to6wa17@gmail.com>
2023-06-08 17:34:29 +0900

    cnoreabbrevするやつ追加した

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-doc-name
 local M = {}
 -- ref:https://docs.coronalabs.com/tutorial/data/outputTable/index.html
 --- dump table 
@@ -5,6 +6,7 @@ local M = {}
 function M.dumpTable(t)
   local printTable_cache = {}
 
+---@diagnostic disable-next-line: redefined-local
   local function sub_printTable( t, indent )
 
     if ( printTable_cache[tostring(t)] ) then
@@ -46,6 +48,42 @@ function M.on_attach(on_attach)
       on_attach(client, buffer)
     end,
   })
+end
+
+-- ref: https://github.com/monaqa/dotfiles/blob/8f7766f142693e47fbef80d6cc1f02fda94fac76/.config/nvim/lua/rc/abbr.lua
+---@param rules abbrrule[]
+function M.make_abbrev(rules)
+    -- 文字列のキーに対して常に0のvalue を格納することで、文字列の hashset を実現。
+    ---@type table<string, abbrrule[]>
+    local abbr_dict_rule = {}
+
+    for _, rule in ipairs(rules) do
+        local key = rule['from']
+        if abbr_dict_rule[key] == nil then
+            abbr_dict_rule[key] = {}
+        end
+        table.insert(abbr_dict_rule[key], rule)
+    end
+
+    for key, rules_with_key in pairs(abbr_dict_rule) do
+        ---コマンドラインが特定の内容だったら、それに対応する値を返す。
+        ---@type table<string, string>
+        local d = {}
+
+        for _, rule in ipairs(rules_with_key) do
+            local required_pattern = rule['from']
+            if rule['prepose_nospace'] ~= nil then
+                required_pattern = rule['prepose_nospace'] .. required_pattern
+            elseif rule['prepose'] ~= nil then
+                required_pattern = rule['prepose'] .. " " .. required_pattern
+            end
+            d[required_pattern] = rule['to']
+        end
+
+        vim.cmd(([[
+        cnoreabbrev <expr> %s (getcmdtype()==# ":") ? get(%s, getcmdline(), %s) : %s
+        ]]):format(key, vim.fn.string(d), vim.fn.string(key), vim.fn.string(key)))
+    end
 end
 
 return M
