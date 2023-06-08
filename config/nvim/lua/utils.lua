1d250671aa1cf59ef850057ae6eca291d15026fa
staticWagomU <to6wa17@gmail.com>
2023-06-08 16:58:40 +0900

    dumptableを更新

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,19 +1,51 @@
 local M = {}
-
+-- ref:https://docs.coronalabs.com/tutorial/data/outputTable/index.html
 --- dump table 
 ---@param t table
----@return string
 function M.dumpTable(t)
-  if type(t) == 'table' then
-    local s = '{ '
-    for k,v in pairs(t) do
-      if type(k) ~= 'number' then k = '"'..k..'"' end
-      s = s .. '['..k..'] = ' .. M.dump(v) .. ','
+  local printTable_cache = {}
+
+  local function sub_printTable( t, indent )
+
+    if ( printTable_cache[tostring(t)] ) then
+      print( indent .. "*" .. tostring(t) )
+    else
+      printTable_cache[tostring(t)] = true
+      if ( type( t ) == "table" ) then
+        for pos,val in pairs( t ) do
+          if ( type(val) == "table" ) then
+            print( indent .. "[" .. pos .. "] => " .. tostring( t ).. " {" )
+            sub_printTable( val, indent .. string.rep( " ", string.len(pos)+8 ) )
+            print( indent .. string.rep( " ", string.len(pos)+6 ) .. "}" )
+          elseif ( type(val) == "string" ) then
+            print( indent .. "[" .. pos .. '] => "' .. val .. '"' )
+          else
+            print( indent .. "[" .. pos .. "] => " .. tostring(val) )
+          end
+        end
+      else
+        print( indent..tostring(t) )
+      end
     end
-    return s .. '} '
-  else
-    return tostring(t)
   end
+
+  if ( type(t) == "table" ) then
+    print( tostring(t) .. " {" )
+    sub_printTable( t, "  " )
+    print( "}" )
+  else
+    sub_printTable( t, "  " )
+  end
+end
+
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
 end
 
 return M
