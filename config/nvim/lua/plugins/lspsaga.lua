348fb8f3bb78728ecf2a6bc11c30435e7f5572a8
staticWagomU <to6wa17@gmail.com>
2023-06-08 16:57:25 +0900

    やっとlsp関連のキーマッピングを入れた

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,14 +1,35 @@
 return {
   "glepnir/lspsaga.nvim",
   event = "LspAttach",
-  opts = {
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+    { "nvim-treesitter/nvim-treesitter" }
+  },
+  config = function()
+    require("lspsaga").setup({
       symbol_in_winbar = {
         enable = false,
         show_file = false,
       },
-  },
-  dependencies = {
-    { "nvim-tree/nvim-web-devicons" },
-    { "nvim-treesitter/nvim-treesitter" }
-  }
+    })
+
+    require("utils").on_attach(function()
+      vim.keymap.set("n", "K", function()
+        local ft = vim.o.filetype
+        if ft == "vim" or ft == "help" then
+          vim.cmd([[execute "h " . expand("<cword>") ]])
+        else
+          vim.cmd([[Lspsaga hover_doc]])
+        end
+      end)
+
+      vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>")
+      vim.keymap.set("n", "gD", "<cmd>Lspsaga goto_definition<cr>")
+
+      vim.keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<cr>")
+      vim.keymap.set("n", "gT", "<cmd>Lspsaga goto_type_definition<cr>")
+
+      vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<cr>")
+    end)
+  end
 }
