e950ce08fd839019985e271aad158a9e88afc5d6
staticWagomU <to6wa17@gmail.com>
2023-06-09 09:12:22 +0900

    indentline ついか

---
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+vim.g["indent_blankline_use_treesitter"] = true
+vim.g["indent_blankline_enabled"] = false
+
+require("utils").make_abbrev({
+  { from = "ib", to = "IndentBlanklineToggle" },
+})
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    show_end_of_line = true,
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+  },
+}
