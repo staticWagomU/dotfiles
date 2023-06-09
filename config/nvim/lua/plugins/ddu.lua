0705a1cf252b15bb37028a5806df4a3cf09a184c
staticWagomU <to6wa17@gmail.com>
2023-06-09 14:09:42 +0900

    っづ

---
+++ config/nvim/lua/plugins/ddu.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "Shougo/ddu.vim",
+    dependencies = {
+      "vim-denops/denops.vim",
+      -- ui
+      "Shougo/ddu-ui-ff",
+      -- kind
+      "Shougo/ddu-kind-file",
+      -- source
+      "Shougo/ddu-source-file",
+      "Shougo/ddu-source-file_rec",
+      { dir = "C:/dev/staticWagomU/ddu-source-keymap" },
+      -- filter
+      "Shougo/ddu-filter-matcher_substring",
+    },
+    config = function()
+    end
+  }
+}
+
