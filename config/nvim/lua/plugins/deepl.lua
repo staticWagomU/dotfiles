41bf9890757e5f7e1a5da256111cbe134c4e08cf
staticWagomU <to6wa17@gmail.com>
2023-06-15 08:25:17 +0900

    deepl追加したよ

---
+++ config/nvim/lua/plugins/deepl.lua
@@ -0,0 +1,18 @@
+return {
+  "ryicoh/deepl.vim",
+  config = function()
+    vim.g["deepl#endpoint"] = "https://api-free.deepl.com/v2/translate"
+    local DEEPL_AUTHKEY = require("local_vimrc").DEEPL_AUTHKEY
+    vim.g["deepl#auth_key"] = DEEPL_AUTHKEY
+    vim.cmd[[
+    " replace a visual selection
+    vmap t<C-e> <Cmd>call deepl#v("EN")<CR>
+    vmap t<C-j> <Cmd>call deepl#v("JA")<CR>
+
+    " translate a current line and display on a new line
+    nmap t<C-e> yypV<Cmd>call deepl#v("EN")<CR>
+    nmap t<C-j> yyp<Cmd>s/`/'/g<CR>V<Cmd>call deepl#v("JA")<CR>
+
+    ]]
+  end
+}
