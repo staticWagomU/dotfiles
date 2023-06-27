8b71f31d400f9571fa90214443b4a9dd7d309e16
staticWagomU <to6wa17@gmail.com>
2023-06-27 16:22:41 +0900

    キーマップ設定してみる？

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,18 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = function()
-      return {
-        signcolumn = true,
-        numhl = true,
-        attach_to_untracked = true,
-        on_attach = function()
-        end,
-      }
-    end
-  },
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup({
+      signcolumn = true,
+      numhl = true,
+      attach_to_untracked = true,
+      on_attach = function()
+      end,
+    })
+
+    vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<cr>", {noremap = true, silent = true})
+    vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<cr>", {noremap = true, silent = true})
+    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", {noremap = true, silent = true})
+    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns stage_hunk<cr>", {noremap = true, silent = true})
+    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns undo_stage_hunk<cr>", {noremap = true, silent = true})
+  end,
 }
