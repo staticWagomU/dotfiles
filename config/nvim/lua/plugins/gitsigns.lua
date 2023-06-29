566327f8adcbda576138338b688d90f9c67598ad
staticWagomU <to6wa17@gmail.com>
2023-06-29 16:37:04 +0900

    りふぁくたりんぐ

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -9,10 +9,11 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<cr>", {noremap = true, silent = true})
-    vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<cr>", {noremap = true, silent = true})
-    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", {noremap = true, silent = true})
-    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns stage_hunk<cr>", {noremap = true, silent = true})
-    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns undo_stage_hunk<cr>", {noremap = true, silent = true})
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "[g", "<cmd>Gitsigns prev_hunk<cr>", opts)
+    vim.keymap.set("n", "]g", "<cmd>Gitsigns next_hunk<cr>", opts)
+    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", opts)
+    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns stage_hunk<cr>", opts)
+    vim.keymap.set("n", "<leader>gss", "<cmd>Gitsigns undo_stage_hunk<cr>", opts)
   end,
 }
