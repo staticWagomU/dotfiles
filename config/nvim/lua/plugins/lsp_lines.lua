80e49f013d8a6dfade2711d0d7e4366e24b2fe8c
staticWagomU <to6wa17@gmail.com>
2023-07-02 19:58:41 +0900

    lsp_linesのsetup書いてなかった

--- config/nvim/lua/plugins/lsp_lines.lua
+++ config/nvim/lua/plugins/lsp_lines.lua
@@ -1,12 +1,12 @@
 return {
-  "Maan2003/lsp_lines.nvim",
-  opts = {},
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  event = "LspAttach",
   config = function()
+    require("lsp_lines").setup()
     -- Disable virtual_text since it's redundant due to lsp_lines.
     vim.diagnostic.config({
       virtual_text = false,
     })
-
     vim.keymap.set(
       "n",
       "<Leader>l",
