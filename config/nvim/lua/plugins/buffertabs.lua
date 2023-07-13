254746540c16eb709da98fedd90f903b6876b291
staticWagomU <to6wa17@gmail.com>
2023-07-13 21:25:30 +0900

    試験導入

---
+++ config/nvim/lua/plugins/buffertabs.lua
@@ -0,0 +1,15 @@
+return {
+  "tomiis4/BufferTabs.nvim",
+  opts = {
+    ---@type 'none'|'single'|'double'|'rounded'|'solid'|'shadow'|table
+    border = 'rounded',
+    ---@type table<string>
+    exclude = { 'NvimTree', 'help', 'dashboard', 'lir', 'alpha' },
+    ---@type 'row'|'column'
+    display = 'column',
+    ---@type 'left'|'right'|'center'
+    horizontal = 'right',
+    ---@type 'top'|'bottom'|'center'
+    vertical = 'top',
+  }
+}
