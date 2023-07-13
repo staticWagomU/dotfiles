fe8c1f8eb61f221e3b3a0caf97c4d0461a47e5fb
staticWagomU <to6wa17@gmail.com>
2023-07-13 21:26:52 +0900

    ちょっと変えた

--- config/nvim/lua/plugins/lspsaga.lua
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -1,10 +1,11 @@
 require("utils").make_abbrev({
   { from = "ldi", to = "Lspsaga show_line_diagnostics" },
   { from = "lca", to = "Lspsaga code_action" },
+  { from = "lgo", to = "Lspsaga outline" },
 })
 
 return {
-  "glepnir/lspsaga.nvim",
+  "nvimdev/lspsaga.nvim",
   event = "LspAttach",
   dependencies = {
     { "nvim-tree/nvim-web-devicons" },
@@ -12,23 +13,27 @@ return {
   },
   config = function()
     require("lspsaga").setup({
-      -- symbol_in_winbar = {
-      --   enable = false,
-      --   show_file = false,
-      -- },
+      symbol_in_winbar = {
+        enable = false,
+        show_file = false,
+      },
+      ui = {
+        code_action = '🌞'
+      }
     })
 
     require("utils").on_attach(function()
-      vim.keymap.set("n", "K", function()
-        local ft = vim.lo.filetype
-        if ft == "vim" or ft == "help" then
-          vim.cmd.execute[["h " . expand("<cword>")]]
-        else
-          vim.cmd.Lspsaga[["hover_doc"]]
-        end
-      end)
+      -- vim.keymap.set("n", "K", function()
+      --   local ft = vim.lo.filetype
+      --   if ft == "vim" or ft == "help" then
+      --     vim.cmd.execute[["h " . expand("<cword>")]]
+      --   else
+      --     vim.cmd[[Lspsaga hover_doc]]
+      --   end
+      -- end)
 
       local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
       vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>", opts)
       vim.keymap.set("n", "gD", "<cmd>Lspsaga goto_definition<cr>", opts)
       vim.keymap.set("n", "gvd", "<cmd>vs | Lspsaga goto_definition<cr>", opts)
@@ -40,6 +45,7 @@ return {
       vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<cr>", opts)
       vim.keymap.set("n", "[l", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
       vim.keymap.set("n", "]l", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+      vim.keymap.set({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<cr>", opts)
     end)
   end
 }
