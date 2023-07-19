2d22a7d6eea97ee3b9df14849dcd0b50ca292dee
staticWagomU <to6wa17@gmail.com>
2023-07-19 23:25:14 +0900

    Other復活

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -1,21 +1,30 @@
 local sveltekit_target = {
   { target = "/%1/%+%2.svelte", context = "view" },
-  { target = "/%1/%+%2.astro", context = "view" },
   { target = "/%1/%+%2\\(*.ts\\|*.js\\)", context = "script", transform = "remove_server" },
   { target = "/%1/%+%2\\(*.ts\\|*.js\\)", context = "script", transform = "add_server" },
 }
 
 return {
   "rgroli/other.nvim",
-  opts = {
+  config = function ()
     require("other-nvim").setup({
       mappings = {
-        -- builtin mappings
         "livewire",
         "angular",
         "laravel",
         "rails",
         "golang",
+        -- astro
+        {
+          pattern = "/src/components/.*.astro$",
+          target = "/src/pages/.*.astro$",
+          context = "pages",
+        },
+        {
+          pattern = "/src/pages/.*.astro",
+          target = "/src/pages/.*.astro",
+          context = "pages",
+        },
         -- sveltekit
         {
           pattern = "/(.*)/%+(.*).server.ts$",
@@ -34,34 +43,21 @@ return {
           target = sveltekit_target,
         },
         {
-          pattern = "/(.*)/%+(.*).(svelte|astro)$",
+          pattern = "/(.*)/%+(.*).(svelte)$",
           target = sveltekit_target,
         },
       },
       transformers = {
-        -- defining a custom transformer
         lowercase = function (inputString)
           return inputString:lower()
         end
       },
       style = {
-        -- How the plugin paints its window borders
-        -- Allowed values are none, single, double, rounded, solid and shadow
         border = "solid",
-
-        -- Column seperator for the window
         seperator = "|",
-
-        -- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
         width = 0.7,
-
-        -- min height in rows.
-        -- when more columns are needed this value is extended automatically
         minHeight = 2
       },
     })
-  },
-  config = function (_, opts)
-    require("other-nvim").setup(opts)
   end
 }
