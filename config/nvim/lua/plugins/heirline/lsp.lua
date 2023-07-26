04dbd7624b60645ac4a7632b0997225e549ed3f8
staticWagomU <to6wa17@gmail.com>
2023-07-26 10:39:59 +0900

    シンプルに書きかえ

--- config/nvim/lua/plugins/heirline/lsp.lua
+++ config/nvim/lua/plugins/heirline/lsp.lua
@@ -8,27 +8,14 @@ return {
   },
   {
     update = { 'LspAttach', 'LspDetach', 'WinEnter' },
-    provider  = function()
-      local names = {}
-      for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-      if server.name == 'null-ls' then
-        local nulllsNames = {}
-        -- null-lsの場合は実際に使っているソースを羅列して表示する
-        table.insert(names, ('null-ls(%s)'):format(table.concat(
-        vim
-        .iter(require('null-ls.sources').get_available(vim.bo.filetype))
-        :map(function(source)
-          table.insert(names, source.name)
-        end)
-        :totable(),
-        ','
-        )))
-      else
-        -- null-ls以外の場合はサーバー名をそのまま表示する
-        table.insert(names, server.name)
-      end
-      end
-      return table.concat(names, ", ")
+    provider = function()
+      local servers = vim
+      .iter(vim.lsp.get_active_clients({ bufnr = 0 }))
+      :map(function(server)
+          return server.name
+      end)
+      :totable()
+      return table.concat(servers, ', ')
     end,
     hl = { fg = colors.blue, bg = colors.slate_blue },
   },
