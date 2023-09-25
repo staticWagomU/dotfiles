778e4bb69a6e3ea5a88058d6d3b76fdaca827f9d
staticWagomU <to6wa17@gmail.com>
2023-09-25 20:22:55 +0900

    最低限のフォーマッター追加

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,21 @@
 return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  opts = {},
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform will run multiple formatters sequentially
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      astro = { { "prettierd", "prettier" } },
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ["_"] = { "trim_whitespace" },
+    },
+    format_on_save = {
+      -- I recommend these options. See :help conform.format for details.
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+  },
 }
