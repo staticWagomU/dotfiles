5105ff622ad503b724de8c714857874770660bcb
staticWagomU <to6wa17@gmail.com>
2023-09-26 23:22:07 +0900

    oilバッファ内でパーミッションとかみれるようにした

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -6,6 +6,21 @@ return {
     require("oil").setup({})
     vim.keymap.set("n", "<leader>e", "<cmd>Oil .<cr>", { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>E", "<cmd>Oil %:p:h<cr>", { noremap = true, silent = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function()
+        vim.keymap.set("n", "<leader>we", function()
+          local oil = require("oil")
+          local config = require("oil.config")
+          if #config.columns == 1 then
+            oil.set_columns({ "icon", "permissions", "size", "mtime" })
+          else
+            oil.set_columns({ "icon" })
+          end
+        end, { noremap = true, silent = true, buffer = true })
+      end,
+    }
+    )
   end,
   event = { "BufReadPre", "BufNewFile" },
 }
