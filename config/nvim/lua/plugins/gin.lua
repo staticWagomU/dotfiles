62fecaac6288795de41a3dc6371d45e6957c16d1
staticWagomU <to6wa17@gmail.com>
2023-09-27 00:06:14 +0900

    ブランチつくるやつ追加した

--- config/nvim/lua/plugins/gin.lua
+++ config/nvim/lua/plugins/gin.lua
@@ -1,19 +1,23 @@
 require("utils").make_abbrev({
-  { from = "git", to = "Gin" },
-  { from = "gin", to = "Gin" },
-  { from = "gs", to = "GinStatus" },
-  { from = "gb", to = "GinBranch" },
-  { from = "gp", to = "Gin push" },
-  { from = "gpp", to = "Gin pull" },
-  { from = "gsh", to = "Gin show" },
-  { from = "gll", to = "GinLog --graph" },
-  { from = "gl", to = "GinLog --graph --oneline" },
-  { from = "gc", to = "Gin commit" },
-  { from = "gd", to = "GinDiff" },
-  { from = "gds", to = "GinDiff --cached" },
+  { from = "git",  to = "Gin" },
+  { from = "gin",  to = "Gin" },
+  { from = "gs",   to = "GinStatus" },
+  { from = "gb",   to = "GinBranch" },
+  { from = "gp",   to = "Gin push" },
+  { from = "gpp",  to = "Gin pull" },
+  { from = "gsh",  to = "Gin show" },
+  { from = "gll",  to = "GinLog --graph" },
+  { from = "gl",   to = "GinLog --graph --oneline" },
+  { from = "gc",   to = "Gin commit" },
+  { from = "gd",   to = "GinDiff" },
+  { from = "gds",  to = "GinDiff --cached" },
   { from = "gdss", to = "GinDiff stash@{}<Left>" },
-  { from = "gss", to = "Gin stash " },
-  { from = "gsl", to = "Gin stash list" },
+  { from = "gss",  to = "Gin stash " },
+  { from = "gsl",  to = "Gin stash list" },
+  { from = "gsc",  to = "Gin switch -C " },
+})
+
+require("utils").make_abbrev({
   { prepose = "Gin commit", from = "a", to = "--amend" },
 })
 
