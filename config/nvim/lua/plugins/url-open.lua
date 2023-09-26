cc98d7db5080b051044415b779765c3dbc1c5726
staticWagomU <to6wa17@gmail.com>
2023-09-26 16:12:27 +0900

    つかうかわからないけど、URL開けると便利じゃない？

---
+++ config/nvim/lua/plugins/url-open.lua
@@ -0,0 +1,14 @@
+return {
+  "sontungexpt/url-open",
+  event = "VeryLazy",
+  cmd = "URLOpenUnderCursor",
+  config = function()
+    local status_ok, url_open = pcall(require, "url-open")
+    if not status_ok then
+      return
+    end
+    url_open.setup({
+      open_only_when_cursor_un_url = true,
+    })
+  end,
+}
