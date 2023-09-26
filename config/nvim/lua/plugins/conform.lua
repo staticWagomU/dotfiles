2377532b4a1a199ede4eeb68aa21213c9142acde
staticWagomU <to6wa17@gmail.com>
2023-09-26 22:55:31 +0900

    update

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,7 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
