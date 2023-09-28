b904bdf475d3684a00ef76d09c44838c9f9e1e64
staticWagomU <to6wa17@gmail.com>
2023-09-28 13:59:51 +0900

    update: ver3 へ移行

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -2,16 +2,18 @@ vim.g["indent_blankline_use_treesitter"] = true
 vim.g["indent_blankline_enabled"] = false
 
 require("utils").make_abbrev({
-  { from = "ib", to = "IndentBlanklineToggle" },
+  { from = "ib", to = "IBLToggle" },
 })
 
 return {
   "lukas-reineke/indent-blankline.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {
-    show_end_of_line = true,
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-  },
+  event = "VeryLazy",
+  config = function()
+    require("ibl").setup({
+      indent = {
+        char = "│",
+      }
+    })
+  end,
 }
