63edbb3c734494bb9a3adffc239a5b8a50a9f447
staticWagomU <to6wa17@gmail.com>
2023-10-12 21:50:13 +0900

    fold関連のやつついか

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,5 +7,7 @@ return {
       ensure_installed = { "astro", "css", "glimmer", "graphql", "html", "javascript", "lua", "php", "python", "scss",
         "svelte", "tsx", "typescript", "vim", "vue", },
     })
+    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
   end,
 }
