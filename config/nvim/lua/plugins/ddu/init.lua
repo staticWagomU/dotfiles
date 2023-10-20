299fed538e9534636bbc205b85aac62a9723c6cd
staticWagomU <to6wa17@gmail.com>
2023-10-20 13:14:29 +0900

    dduきたよ

--- config/nvim/lua/plugins/ddu/init.lua
+++ config/nvim/lua/plugins/ddu/init.lua
@@ -4,10 +4,14 @@ return {
     "vim-denops/denops.vim",
     "lambdalisue/kensaku.vim",
     "Shougo/ddu-commands.vim",
-    -- column
+    ------------------------------
+    -- | column                   |
+    ------------------------------
     "Shougo/ddu-column-filename",
     "tamago3keran/ddu-column-devicon_filename",
-    -- filter
+    ------------------------------
+    -- | filter                   |
+    ------------------------------
     "Milly/ddu-filter-kensaku",
     "Shougo/ddu-filter-matcher_files",
     "Shougo/ddu-filter-matcher_relative",
@@ -17,12 +21,16 @@ return {
     "kamecha/ddu-filter-converter_file_icon",
     "kamecha/ddu-filter-converter_file_info",
     "uga-rosa/ddu-filter-converter_devicon",
-    -- kind
+    ------------------------------
+    -- | kind                     |
+    ------------------------------
     "Shougo/ddu-kind-file",
     "Shougo/ddu-kind-word",
-    -- source
+    ------------------------------
+    -- | source                   |
+    ------------------------------
+    "4513ECHO/ddu-source-colorscheme",
     "4513ECHO/ddu-source-emoji",
-    -- "Milly/windows-clipboard-history.vim",
     "Shougo/ddu-source-dummy",
     "Shougo/ddu-source-file",
     "Shougo/ddu-source-file_old",
@@ -44,8 +52,100 @@ return {
     "shun/ddu-source-rg",
     "uga-rosa/ddu-source-lsp",
     "uga-rosa/ddu-source-search_history",
-    -- ui
+    ------------------------------
+    -- | ui                       |
+    ------------------------------
     "Shougo/ddu-ui-ff",
     "Shougo/ddu-ui-filer",
   },
+  config = function()
+    vim.fn["ddu#custom#patch_global"]({
+        ui = "ff",
+        uiParams = {
+          ff = {
+            filterFloatingPosition = "bottom",
+            filterSplitDirection = "floating",
+            floatingBorder = "rounded",
+            previewFloating = true,
+            previewFloatingBorder = "rounded",
+            previewFloatingTitle = "Preview",
+            previewSplit = "horizontal",
+            prompt = "> ",
+            split = "floating",
+            startFilter = true,
+          }
+        },
+        sourceOptions = {
+          _ = {
+            matchers = {
+              "matcher_substring",
+            },
+            ignoreCase = true,
+          },
+        },
+      })
+
+    vim.fn["ddu#custom#patch_local"]("file_recursive", {
+        sources = {
+          {
+            name = { "file_rec" },
+            options = {
+              matchers = {
+                "matcher_substring",
+              },
+              converters = {
+                "converter_devicon",
+              },
+              ignoreCase = true,
+            },
+            params = {
+              ignoredDirectories = { "node_modules", ".git", ".vscode" },
+            },
+          },
+        },
+        kindOptions = {
+          file = {
+            defaultAction = "open",
+          }
+        }
+      })
+
+    vim.fn["ddu#custom#patch_local"]("colorscheme", {
+        sources = {
+          {
+            name = { "colorscheme" },
+            options = {
+              matchers = {
+                "matcher_substring",
+              },
+            },
+          },
+        },
+        kindOptions = {
+          colorscheme = {
+            defaultAction = "set",
+          }
+        }
+      })
+
+
+    vim.api.nvim_create_autocmd("FileType",{
+        pattern = "ddu-ff",
+        callback = function()
+          local opts = { noremap = true, silent = true, buffer = true }
+          vim.keymap.set({ "n" }, "q", [[<Cmd>call ddu#ui#ff#do_action("quit")<CR>]], opts)
+          vim.keymap.set({ "n" }, "<Cr>", [[<Cmd>call ddu#ui#ff#do_action("itemAction")<CR>]], opts)
+          vim.keymap.set({ "n" }, "i", [[<Cmd>call ddu#ui#ff#do_action("openFilterWindow")<CR>]], opts)
+          vim.keymap.set({ "n" }, "P", [[<Cmd>call ddu#ui#ff#do_action("togglePreview")<CR>]], opts)
+        end,
+      })
+
+    vim.api.nvim_create_autocmd("FileType",{
+        pattern = "ddu-ff-filter",
+        callback = function()
+          local opts = { noremap = true, silent = true, buffer = true }
+          vim.keymap.set({ "n", "i" }, "<CR>", [[<Esc><Cmd>close<CR>]], opts)
+        end,
+      })
+  end,
 }
