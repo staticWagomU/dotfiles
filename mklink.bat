5e938b6e4d65cefad144ebb7f0c25201134d9018
staticWagomU <to6wa17@gmail.com>
2023-10-23 08:22:50 +0900

    vimへのリンク方法を変更

--- mklink.bat
+++ mklink.bat
@@ -1,8 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-for /F %%A in ('dir .\config\vim /B') do (
-  mklink /H %USERPROFILE%\vimfiles\%%A %USERPROFILE%\dotfiles\config\vim\%%A
-)
-endlocal
 
 mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotfiles\config\nvim
+mklink /D C:\Users\wagomu\vimfiles C:\Users\wagomu\dotfiles\config\vim
