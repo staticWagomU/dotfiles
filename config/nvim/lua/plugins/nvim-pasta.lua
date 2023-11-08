89f87964a35311a2e7e096ac24e067bcb68a050e
staticWagomU <to6wa17@gmail.com>
2023-11-08 13:41:06 +0900

    pasteきりかえられるようにする

---
+++ config/nvim/lua/plugins/nvim-pasta.lua
@@ -0,0 +1,12 @@
+return {
+  "hrsh7th/nvim-pasta",
+  config = function()
+    vim.keymap.set({ 'n', 'x' }, 'p', require('pasta.mapping').p)
+    vim.keymap.set({ 'n', 'x' }, 'P', require('pasta.mapping').P)
+
+    require('pasta').config.next_key = vim.keycode('<C-n>')
+    require('pasta').config.prev_key = vim.keycode('<C-p>')
+    require('pasta').config.indent_key = vim.keycode(',')
+    require('pasta').config.indent_fix = true
+  end,
+}
