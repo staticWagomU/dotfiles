37377be5d5747120211e66ba86efdfa6b83cf442
staticWagomU <to6wa17@gmail.com>
2023-11-20 16:18:04 +0900

    上品な暮らし

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,29 @@
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings= 1
+vim.g.loaded_netrwFileHandlers = 1
+
+vim.g.mapleader = " "
+
+vim.opt.compatible = false
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.helplang = "ja"
+vim.opt.signcolumn = "yes"
+vim.opt.hidden = true
+vim.opt.laststatus = 3
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus", "autoselect"
+
