eb9b0269888b67b6b9c2f0fedfb664ea3a5a2e6b
staticWagomU <to6wa17@gmail.com>
2023-11-21 21:24:13 +0900

    update

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,76 +1,68 @@
-local vim                      = vim
-vim.g.loaded_gzip              = 1
-vim.g.loaded_tar               = 1
-vim.g.loaded_tarPlugin         = 1
-vim.g.loaded_zip               = 1
-vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_rrhelper          = 1
-vim.g.loaded_2html_plugin      = 1
-vim.g.loaded_vimball           = 1
-vim.g.loaded_vimballPlugin     = 1
-vim.g.loaded_getscript         = 1
-vim.g.loaded_getscriptPlugin   = 1
-vim.g.loaded_netrw             = 0
-vim.g.loaded_netrwPlugin       = 0
-vim.g.loaded_netrwSettings     = 0
-vim.g.loaded_netrwFileHandlers = 0
-vim.o.foldcolumn               = '0'
-vim.o.foldlevel                = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart           = 99
-vim.o.foldenable               = true
-vim.o.shiftwidth               = 2
-vim.o.tabstop                  = 2
-vim.o.softtabstop              = 2
-vim.g.mapleader                = " "
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings= 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 
-local set                      = vim.opt
 
-set.number                     = false
-set.relativenumber             = false
-set.wrap                       = false
-set.helplang                   = "ja"
-set.signcolumn                 = "yes"
-set.hidden                     = true
-set.laststatus                 = 3
-set.mouse                      = "a"
-set.clipboard                  = "unnamedplus"
-set.ambiwidth                  = "single"
-set.ignorecase                 = true
-set.smartcase                  = true
-set.splitbelow                 = true
-set.splitright                 = true
-set.display                    = "uhex"
-set.wildmenu                   = true
-set.expandtab                  = true
-set.wrapscan                   = true
-set.termguicolors              = true
-set.showmode                   = false
-set.list                       = true
-set.listchars                  = { eol = '↴', tab = '▸ ', trail = '»', space = '⋅' }
-set.fillchars                  = {
-  stl = '━',
-  -- stl = ' ',
-  stlnc = ' ',
-  diff = '∙',
-  eob = ' ',
-  fold = '·',
-  horiz = '━',
-  horizup = '┻',
-  horizdown = '┳',
-  vert = '┃',
-  vertleft = '┫',
-  vertright = '┣',
-  verthoriz = '╋'
+local set = vim.opt
+set.ambiwidth = "single"
+set.backspace = { "indent", "eol", "start" }
+set.backupdir = vim.fn.expand("~")
+set.clipboard = "unnamedplus"
+set.directory = vim.fn.expand("~")
+set.display = "uhex"
+set.expandtab = true
+set.expandtab = true
+set.fileformat = "unix"
+set.fillchars = {
+	stl = '━',
+	stlnc = ' ',
+	diff = '∙',
+	eob = ' ',
+	fold = '·',
+	horiz = '━',
+	horizup = '┻',
+	horizdown = '┳',
+	vert = '┃',
+	vertleft = '┫',
+	vertright = '┣',
+	verthoriz = '╋'
 }
-set.backspace                  = { "indent", "eol", "start" }
----@diagnostic disable-next-line: assign-type-mismatch
-set.directory                  = vim.fn.expand("~")
----@diagnostic disable-next-line: assign-type-mismatch
-set.backupdir                  = vim.fn.expand("~")
----@diagnostic disable-next-line: assign-type-mismatch
-set.undodir                    = vim.fn.expand("~")
-set.laststatus                 = 3
-set.fileformat                 = "unix"
+set.helplang = "ja"
+set.hidden = true
+set.ignorecase = true
+set.laststatus = 3
+set.list = true
+set.listchars = { eol = '↴', tab = '▸ ', trail = '»', space = '⋅' }
+set.mouse = "a"
+set.shiftwidth = 2
+set.showcmd = false
+set.showmode = false
+set.signcolumn = "yes"
+set.smartcase = true
+set.smarttab = true
+set.softtabstop = 2
+set.splitbelow = true
+set.splitright = true
+set.swapfile = false
+set.tabstop = 2
+set.termguicolors = true
+set.undodir = vim.fn.expand("~")
+set.whichwrap = "b,s,h,l,<,>,[,]"
+set.wildmenu = true
+set.wrap = false
+set.wrapscan = true
 
 vim.cmd [=[
 augroup restore-cursor
@@ -100,14 +92,6 @@ vim.filetype.add({
   },
 })
 
--- vim.api.nvim_create_autocmd("InsertEnter", {
---   pattern = "*",
---   desc = "インサート時に検索ハイライトを消す",
---   callback = function()
---     vim.schedule(vim.cmd.nohlsearch)
---   end
--- })
-
 vim.api.nvim_create_augroup("extra-whitespace", {})
 vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter" }, {
   group = "extra-whitespace",
@@ -120,19 +104,4 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
   pattern = { "*" },
   desc = "全角空白を可視化させる",
   command = [[highlight default ExtraWhitespace ctermbg=red guibg=red]]
-})
-local lsplns = vim.api.nvim_create_augroup("toggleLspLines", {})
-vim.api.nvim_create_autocmd("InsertEnter", {
-  group = lsplns,
-  pattern = { "*" },
-  callback = function()
-    vim.diagnostic.config({ virtual_lines = false })
-  end
-})
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = lsplns,
-  pattern = { "*" },
-  callback = function()
-    vim.diagnostic.config({ virtual_lines = true })
-  end
 })
