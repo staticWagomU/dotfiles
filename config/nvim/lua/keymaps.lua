088cf78739bfec0b4994670e51525160c985b91d
staticWagomU <to6wa17@gmail.com>
2023-11-22 14:13:50 +0900

    fix typo

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -4,6 +4,6 @@ local keymap = vim.keymap.set
 keymap({ "n" }, "<Leader>w", "<Cmd>w<Cr>", opts)
 
 keymap({ "n" }, "<Leader>bn", "<Cmd>bnext<Cr>", opts)
-keymap({ "n" }, "<Leader>bp", "<Cmd>bprevioues<Cr>", opts)
+keymap({ "n" }, "<Leader>bp", "<Cmd>bprevious<Cr>", opts)
 keymap({ "n" }, "<Leader>bd", "<Cmd>bdelete<Cr>", opts)
 
