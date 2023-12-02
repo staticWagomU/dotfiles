b139941bc1643ea187af74f39243fca4721825ca
staticWagomU <to6wa17@gmail.com>
2023-12-02 22:32:59 +0900

    neovim: ついでにこれも

--- config/nvim/rc/dpp.ts
+++ config/nvim/rc/dpp.ts
@@ -1,153 +1,146 @@
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
 import {
-	ensure,
-	is,
-} from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
-import {
-	BaseConfig,
-	ContextBuilder,
-	ConfigReturn,
-	Dpp,
-	Plugin,
+  BaseConfig,
+  ConfigReturn,
+  ContextBuilder,
+  Dpp,
+  Plugin,
 } from "https://deno.land/x/dpp_vim@v0.0.5/types.ts";
-import {
-	Denops,
-} from "https://deno.land/x/dpp_vim@v0.0.5/deps.ts";
+import { Denops } from "https://deno.land/x/dpp_vim@v0.0.5/deps.ts";
 
 type Toml = {
-	hooks_file?: string;
-	ftplugins?: Record<string, string>;
-	plugins?: Plugin[];
+  hooks_file?: string;
+  ftplugins?: Record<string, string>;
+  plugins?: Plugin[];
 };
 
 type LazyMakeStateResult = {
-	plugins: Plugin[];
-	stateLines: string[];
+  plugins: Plugin[];
+  stateLines: string[];
 };
 
 const isStringArray = is.ArrayOf(is.String);
 // https://github.com/kuuote/dotvim/blob/version5/conf/dpp.ts?plain=1#L27-L32
 async function glob(denops: Denops, path: string): Promise<string[]> {
-	return ensure(
-		await denops.call("glob", path, 1, 1),
-		isStringArray,
-	);
+  return ensure(
+    await denops.call("glob", path, 1, 1),
+    isStringArray,
+  );
 }
 
 export class Config extends BaseConfig {
-	override async config(args: {
-		denops: Denops;
-		contextBuilder: ContextBuilder;
-		basePath: string;
-		dpp: Dpp;
-	}): Promise<ConfigReturn> {
-		const dotfilesDir = "~/dotfiles/config/nvim/rc"
-		const tomlPaths = await glob(args.denops, `${dotfilesDir}/*.toml`)
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+    basePath: string;
+    dpp: Dpp;
+  }): Promise<ConfigReturn> {
+    const dotfilesDir = "~/dotfiles/config/nvim/rc";
+    const tomlPaths = await glob(args.denops, `${dotfilesDir}/*.toml`);
 
-		args.contextBuilder.setGlobal({
-			extParams: {
-				installer: {
-					checkDiff: true,
-					logFilePath: "~/.cache/dpp/installer-log.txt",
-				},
-			},
-			protocols: ["git"],
-		});
+    args.contextBuilder.setGlobal({
+      extParams: {
+        installer: {
+          checkDiff: true,
+          logFilePath: "~/.cache/dpp/installer-log.txt",
+        },
+      },
+      protocols: ["git"],
+    });
 
+    const [context, options] = await args.contextBuilder.get(args.denops);
 
-		const [context, options] = await args.contextBuilder.get(args.denops);
+    const tomls: Toml[] = [];
+    for (const tomlPath of tomlPaths) {
+      tomls.push(
+        await args.dpp.extAction(
+          args.denops,
+          context,
+          options,
+          "toml",
+          "load",
+          {
+            path: tomlPath,
+            options: {
+              lazy: false,
+            },
+          },
+        ) as Toml,
+      );
+    }
 
-		const tomls: Toml[] = [];
-		for(const tomlPath of tomlPaths) {
-			tomls.push(
-				await args.dpp.extAction(
-					args.denops,
-					context,
-					options,
-					"toml",
-					"load",
-					{
-						path: tomlPath,
-						options: {
-							lazy: false,
-						},
-					},
-				) as Toml,
-			);
-		}
+    const recordPlugins: Record<string, Plugin> = {};
+    const ftplugins: Record<string, string> = {};
+    const hooksFiles: string[] = [];
 
-		const recordPlugins: Record<string, Plugin> = {};
-		const ftplugins: Record<string, string> = {};
-		const hooksFiles: string[] = [];
+    tomls.forEach((toml) => {
+      for (const plugin of toml.plugins!) {
+        recordPlugins[plugin.name] = plugin;
+      }
 
-		tomls.forEach((toml) => {
+      if (toml.ftplugins) {
+        for (const filetype of Object.keys(toml.ftplugins)) {
+          if (ftplugins[filetype]) {
+            ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
+          } else {
+            ftplugins[filetype] = toml.ftplugins[filetype];
+          }
+        }
+      }
 
-			for (const plugin of toml.plugins!) {
-				recordPlugins[plugin.name] = plugin;
-			}
+      if (toml.hooks_file) {
+        hooksFiles.push(toml.hooks_file);
+      }
+    });
 
-			if (toml.ftplugins) {
-				for (const filetype of Object.keys(toml.ftplugins)) {
-					if (ftplugins[filetype]) {
-						ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
-					} else {
-						ftplugins[filetype] = toml.ftplugins[filetype];
-					}
-				}
-			}
+    const localPlugins = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "local",
+      "local",
+      {
+        directory: "~/dotfiles/config/nvim/plugins",
+        options: {
+          frozen: true,
+          merged: false,
+        },
+        includes: [
+          "ddu-source-patch_local",
+          "denops-statusline",
+        ],
+      },
+    ) as Plugin[] | undefined;
 
-			if (toml.hooks_file) {
-				hooksFiles.push(toml.hooks_file);
-			}
-		});
+    if (localPlugins) {
+      for (const plugin of localPlugins) {
+        if (plugin.name in recordPlugins) {
+          recordPlugins[plugin.name] = Object.assign(
+            recordPlugins[plugin.name],
+            plugin,
+          );
+        } else {
+          recordPlugins[plugin.name] = plugin;
+        }
+      }
+    }
 
-		const localPlugins = await args.dpp.extAction(
-			args.denops,
-			context,
-			options,
-			"local",
-			"local",
-			{
-				directory: "~/dotfiles/config/nvim/plugins",
-				options: {
-					frozen: true,
-					merged: false,
-				},
-				includes: [
-					"ddu-source-patch_local",
-					"denops-statusline",
-				],
-			},
-		) as Plugin[] | undefined;
+    const lazyResult = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult;
 
-		if (localPlugins) {
-			for (const plugin of localPlugins) {
-				if (plugin.name in recordPlugins) {
-					recordPlugins[plugin.name] = Object.assign(
-						recordPlugins[plugin.name],
-						plugin,
-					);
-				} else {
-					recordPlugins[plugin.name] = plugin;
-				}
-			}
-		}
-
-		const lazyResult = await args.dpp.extAction(
-			args.denops,
-			context,
-			options,
-			"lazy",
-			"makeState",
-			{
-				plugins: Object.values(recordPlugins),
-			},
-		) as LazyMakeStateResult;
-
-		return {
-			ftplugins,
-			hooksFiles,
-			plugins: lazyResult.plugins ?? [],
-			stateLines: lazyResult.stateLines,
-		};
-	}
+    return {
+      ftplugins,
+      hooksFiles,
+      plugins: lazyResult.plugins ?? [],
+      stateLines: lazyResult.stateLines,
+    };
+  }
 }
