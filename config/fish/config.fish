0d25886a8582ea9b634cb63252e18faa22b3bd75
staticWagomU <to6wa17@gmail.com>
2023-12-04 14:09:47 +0900

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+fish_add_path ~/bin
+
 # aqua
 set -xg AQUA_ROOT_DIR $HOME/.local/share/aquaproj-aqua
 fish_add_path $AQUA_ROOT_DIR/bin
@@ -15,3 +17,5 @@ fish_add_path $VOLTA_HOME/bin
 # Alias
 # -------------------------
 alias cls clear
+
+alias tree 'tree -a -I "\.DS_Store|\.git|node_modules|dist|vendor\/bundle" -N'
