9f4286e98757361404344223ce24f9b1225a73bf
staticWagomU <to6wa17@gmail.com>
2023-12-07 17:32:21 +0900

    neovim: stylua

--- config/nvim/lua/dpp_vim.lua
+++ config/nvim/lua/dpp_vim.lua
@@ -1,27 +1,27 @@
-local dpp = require("dpp")
-local abbrev = require("utils").make_abbrev
-local autocmd = require("utils").autocmd
-local uc = require("utils").usercmd
+local dpp = require('dpp')
+local abbrev = require('utils').make_abbrev
+local autocmd = require('utils').autocmd
+local uc = require('utils').usercmd
 
-local dppBase = require("dpp.helper").basePath
-local tsPath = require("dpp.helper").tsPath
+local dppBase = require('dpp.helper').basePath
+local tsPath = require('dpp.helper').tsPath
 
 if dpp.load_state(dppBase) then
-  autocmd("User", {
-    pattern = "DenopsReady",
+  autocmd('User', {
+    pattern = 'DenopsReady',
     callback = function()
-      vim.notify("dpp#make_state start")
+      vim.notify('dpp#make_state start')
       dpp.make_state(dppBase, tsPath)
     end,
   })
 end
 
-autocmd("User", {
-  pattern = "Dpp:makeStatePost",
+autocmd('User', {
+  pattern = 'Dpp:makeStatePost',
   callback = function()
-    vim.notify("dpp#make_state done")
+    vim.notify('dpp#make_state done')
     dpp.source()
-    if require("utils").is_windows then
+    if require('utils').is_windows then
       vim.fn.system([[powershell -Command "rundll32 user32.dll,MessageBeep"]])
     else
       vim.fn.system([[paplay /usr/share/sounds/freedesktop/stereo/complete.oga]])
@@ -29,61 +29,61 @@ autocmd("User", {
   end,
 })
 
-autocmd({ "VimEnter" }, {
-  pattern = "*",
+autocmd({ 'VimEnter' }, {
+  pattern = '*',
   callback = function()
-    require("dpp").source()
+    require('dpp').source()
   end,
 })
 
-uc("DppMakeState", function()
-  vim.notify("dpp#make_state start")
+uc('DppMakeState', function()
+  vim.notify('dpp#make_state start')
   dpp.make_state(dppBase, tsPath)
 end)
-uc("DppLoad", function()
+uc('DppLoad', function()
   dpp.load_state(dppBase)
 end)
-uc("DppInstall", function()
-  dpp.async_ext_action("installer", "install", { maxProcess = 10 })
+uc('DppInstall', function()
+  dpp.async_ext_action('installer', 'install', { maxProcess = 10 })
 end)
-uc("DppUpdate", function()
-  dpp.async_ext_action("installer", "update", { maxProcess = 10 })
+uc('DppUpdate', function()
+  dpp.async_ext_action('installer', 'update', { maxProcess = 10 })
 end)
-uc("DppSource", function()
+uc('DppSource', function()
   dpp.source()
-  vim.notify("dpp#source done")
+  vim.notify('dpp#source done')
 end)
-uc("DppClear", function()
+uc('DppClear', function()
   dpp.clear_state()
 end)
 
-abbrev({
-  { from = "dm", to = "DppMakeState" },
-  { from = "dl", to = "DppLoad" },
-  { from = "di", to = "DppInstall" },
-  { from = "du", to = "DppUpdate" },
-  { from = "ds", to = "DppSource" },
-  { from = "dc", to = "DppClear" },
-})
+abbrev {
+  { from = 'dm', to = 'DppMakeState' },
+  { from = 'dl', to = 'DppLoad' },
+  { from = 'di', to = 'DppInstall' },
+  { from = 'du', to = 'DppUpdate' },
+  { from = 'ds', to = 'DppSource' },
+  { from = 'dc', to = 'DppClear' },
+}
 
-vim.cmd("filetype indent plugin on")
-if vim.fn.has("syntax") then
-  vim.cmd.syntax("on")
+vim.cmd('filetype indent plugin on')
+if vim.fn.has('syntax') then
+  vim.cmd.syntax('on')
 end
 
 -- Neovimの設定ファイルを編集したら、dpp#make_stateを実行する
-autocmd("BufWritePost", {
+autocmd('BufWritePost', {
   callback = function(e)
     local normalize = vim.fs.normalize
     local expand = vim.fn.expand
 
-    local dotnvim = normalize(expand("~/dotfiles/config/nvim"))
+    local dotnvim = normalize(expand('~/dotfiles/config/nvim'))
     local bufname = normalize(e.match)
     if bufname:find(dotnvim, 1, true) == nil then
       return
     end
 
-    vim.notify("dpp#make_state start")
+    vim.notify('dpp#make_state start')
     dpp.make_state(dppBase, tsPath)
   end,
 })
