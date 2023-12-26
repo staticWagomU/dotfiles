bcb4be4f885ba1ec35788a03c85d614e80d0355c
staticWagomU <to6wa17@gmail.com>
2023-12-26 22:39:05 +0900

    wezterm: こんくらいひつようやろ

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
-config.font_size = 11.0
+config.font_size = 14.0
 -- config.font = wezterm.font("JetBrains Mono")
 config.font = wezterm.font("HackGen Console NF")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
