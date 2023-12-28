a2463477b7733e4f8ebc3d4b7d64570cb714e7b9
staticWagomU <to6wa17@gmail.com>
2023-12-28 10:44:46 +0900

    これでいける

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-mklink /D C:\Users\wagomu\AppData\Local\nvim C:\Users\wagomu\dotvim\nvim
+REM nvimのシンボリックリンクを作成
+mklink /D %LOCALAPPDATA%\nvim %USERPROFILE%\dotvim\nvim
+
+REM vimのシンボリックリンクを作成
+REM dir /B /A:Dでディレクトリのシンボリックリンクを作成する
+for /f %%i in ('dir /B /A:D %USERPROFILE%\dotvim\vim') do (
+  mklink /D %USERPROFILE%\vimfiles\%%i %USERPROFILE%\dotvim\vim\%%i
+)
+
+REM dir /B /A-Dでファイルのシンボリックリンクを作成する
+for /f %%i in ('dir /B /A-D %USERPROFILE%\dotvim\vim') do (
+  mklink %USERPROFILE%\vimfiles\%%i %USERPROFILE%\dotvim\vim\%%i
+)
