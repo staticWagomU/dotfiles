eff69c3d71e1f83ded1fb79fc71bd0d780047f7d
staticWagomU <to6wa17@gmail.com>
2024-01-01 22:08:37 +0900

    wezterm: これでいきましょう

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,26 +1,34 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-local config = {}
+-- local config = {}
+local config = wezterm.config_builder()
 config.font_size = 14.0
--- config.font = wezterm.font("JetBrains Mono")
-config.font = wezterm.font("HackGen Console NF")
+config.font = wezterm.font_with_fallback({ "HackGen Console NF" })
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.color_scheme = "iceberg-dark"
-config.use_ime = false
 config.disable_default_key_bindings = true
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 config.keys = {
-	-- paste from the clipboard
-	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-	-- paste from the primary selection
-	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
-	-- vim key binding
-	{ key = "h", mods = "ALT", action = act.SendKey({ key = "LeftArrow" }) },
-	{ key = "j", mods = "ALT", action = act.SendKey({ key = "DownArrow" }) },
-	{ key = "k", mods = "ALT", action = act.SendKey({ key = "UpArrow" }) },
-	{ key = "l", mods = "ALT", action = act.SendKey({ key = "RightArrow" }) },
-}
+    { key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+    { key = "V", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
 
+    { key = "P", mods = "SHIFT|CTRL", action = act.ActivateCommandPalette },
+    { key = "L", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
+    { key = "T", mods = "SHIFT|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "X", mods = "SHIFT|CTRL", action = act.CloseCurrentTab({ confirm = true }) },
+    { key = '"', mods = "SHIFT|CTRL", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+    { key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+    { key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
+
+}
+config.window_padding = {
+    left = "5px",
+    right = 0,
+    top = "5px",
+    bottom = 0,
+}
 return config
