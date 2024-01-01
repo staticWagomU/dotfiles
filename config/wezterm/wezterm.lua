18e75b518a81b83bc991e8e823235f29c26e53af
staticWagomU <to6wa17@gmail.com>
2024-01-01 09:33:49 +0900

    wezterm: update config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,16 +7,20 @@ config.font_size = 14.0
 config.font = wezterm.font("HackGen Console NF")
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.color_scheme = "iceberg-dark"
+config.use_ime = false
+config.disable_default_key_bindings = true
+config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
-  -- paste from the clipboard
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  -- paste from the primary selection
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
-  -- vim key binding
-  { key = 'h', mods = 'ALT', action = act.SendKey { key = 'LeftArrow'}},
-  { key = 'j', mods = 'ALT', action = act.SendKey { key = 'DownArrow'}},
-  { key = 'k', mods = 'ALT', action = act.SendKey { key = 'UpArrow'}},
-  { key = 'l', mods = 'ALT', action = act.SendKey { key = 'RightArrow'}},
+	-- paste from the clipboard
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	-- paste from the primary selection
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
+	-- vim key binding
+	{ key = "h", mods = "ALT", action = act.SendKey({ key = "LeftArrow" }) },
+	{ key = "j", mods = "ALT", action = act.SendKey({ key = "DownArrow" }) },
+	{ key = "k", mods = "ALT", action = act.SendKey({ key = "UpArrow" }) },
+	{ key = "l", mods = "ALT", action = act.SendKey({ key = "RightArrow" }) },
 }
 
 return config
