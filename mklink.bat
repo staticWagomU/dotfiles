6c19fd30d2cbec25dbaaa55f21cf4f056efb4aef
staticWagomU <to6wa17@gmail.com>
2024-01-17 13:03:29 +0900

    update: add del command before mklink

--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,5 @@
+del %USERPROFILE%\.wezterm.lua
 mklink %USERPROFILE%\.wezterm.lua %USERPROFILE%\dotfiles\config\wezterm\wezterm.lua
 
+del %USERPROFILE%\.tigrc
 mklink %USERPROFILE%\.tigrc %USERPROFILE%\dotfiles\config\.tigrc
