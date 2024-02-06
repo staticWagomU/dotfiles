f1148cde080a25d332ee1489ad7170d27d7c05a2
staticWagomU <to6wa17@gmail.com>
2024-02-06 15:54:02 +0900

    fish: 整理

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,10 @@ end
 
 fish_add_path ~/bin
 
+# deno
+set -xg DENO_INSTALL /home/wagomu/.deno
+fish_add_path $DENO_INSTALL/bin
+
 # aqua
 set -xg AQUA_ROOT_DIR $HOME/.local/share/aquaproj-aqua
 fish_add_path $AQUA_ROOT_DIR/bin
@@ -12,10 +16,29 @@ fish_add_path $AQUA_ROOT_DIR/bin
 set -xg VOLTA_HOME $HOME/.volta
 fish_add_path $VOLTA_HOME/bin
 
-
 # -------------------------
-# Alias
+# Abbr
 # -------------------------
-alias cls clear
+abbr -a cls clear
+abbr -a cp cp -i
+abbr -a mc mv -i
+abbr -a rm rm -i
+abbr -a ll ls -al
 
-alias tree 'tree -a -I "\.DS_Store|\.git|node_modules|dist|vendor\/bundle" -N'
+abbr -a gs git status
+abbr -a gb branch
+abbr -a gd git diff
+
+
+function peco_history
+  history|peco --layout=bottom-up|read foo
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
+
+function fish_user_key_bindings
+  bind \cr peco_history
+end
