fbed642e510d36f5714d846fe067b1efabdf0dc8
staticWagomU <to6wa17@gmail.com>
2024-03-09 18:17:04 +0900

    add .gitconfig

--- mklink.sh
+++ mklink.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/config/nvim ~/.config/nvim
 ln -s ~/dotfiles/config/wezterm.lua ~/.config/wezterm/wezterm.lua
 ln -s ~/dotfiles/config/fish ~/.config/fish
 ln -s ~/dotfiles/config/aqua.yaml ~/aqua.yaml
+ln -s ~/dotfiles/config/.gitconfig ~/.gitconfig
