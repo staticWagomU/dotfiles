6d43176035f5f7499a3351f0f4cab6f250d5cddc
staticWagomU <to6wa17@gmail.com>
2024-03-09 16:42:52 +0900

    update ln

--- mklink.sh
+++ mklink.sh
@@ -3,4 +3,4 @@
 ln -s ~/dotfiles/config/nvim ~/.config/nvim
 ln -s ~/dotfiles/config/wezterm.lua ~/.config/wezterm/wezterm.lua
 ln -s ~/dotfiles/config/fish ~/.config/fish
-ln -s ~/dotfiles/aqua.yaml ~/aqua.yaml
+ln -s ~/dotfiles/config/aqua.yaml ~/aqua.yaml
