678c9b13c32e636ef5e9b81acbcd5d2340b65dd2
staticWagomU <to6wa17@gmail.com>
2024-03-12 17:23:22 +0900

    mklinik: update

--- mklink.sh
+++ mklink.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/config/nvim ~/.config/nvim
+rm ~/.config/wezterm/wezterm.lua
 ln -s ~/dotfiles/config/wezterm.lua ~/.config/wezterm/wezterm.lua
-ln -s ~/dotfiles/config/fish ~/.config/fish
+rm ~/.config/fish/config.fish
+ln -s ~/dotfiles/config/fish ~/.config/fish/config.fish
+rm ~/aqua.yaml
 ln -s ~/dotfiles/config/aqua.yaml ~/aqua.yaml
+rm ~/.gitconfig
 ln -s ~/dotfiles/config/.gitconfig ~/.gitconfig
