5dafd52b615bf71db33d05f14c82b0444693f4db
staticWagomU <to6wa17@gmail.com>
2024-03-13 21:15:17 +0900

    mklink: update

--- mklink.sh
+++ mklink.sh
@@ -3,7 +3,9 @@
 rm ~/.config/wezterm/wezterm.lua
 ln -s ~/dotfiles/config/wezterm.lua ~/.config/wezterm/wezterm.lua
 rm ~/.config/fish/config.fish
-ln -s ~/dotfiles/config/fish ~/.config/fish/config.fish
+ln -s ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
+rm -rf ~/.config/fish/functions
+ln -s ~/dotfiles/config/fish/functions ~/.config/fish/functions
 rm ~/aqua.yaml
 ln -s ~/dotfiles/config/aqua.yaml ~/aqua.yaml
 rm ~/.gitconfig
