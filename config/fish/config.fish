8deb12d7a689ba698c0ce54e29001666e8456ed3
staticWagomU <to6wa17@gmail.com>
2024-04-03 21:20:10 +0900

    fish: promptをかっこよくしたよ

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -85,3 +85,30 @@ function fish_user_key_bindings
   bind \cr peco_history
 end
 
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑ "
+set -g __fish_git_prompt_char_upstream_behind "↓ "
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "● "
+set -g __fish_git_prompt_char_dirtystate "✚ "
+set -g __fish_git_prompt_char_untrackedfiles "… "
+set -g __fish_git_prompt_char_conflictedstate "✖ "
+set -g __fish_git_prompt_char_cleanstate "✔ "
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
+
+# 各モードでデフォルトキーバインドを呼び出し
+for mode in default insert visual
+    fish_default_key_bindings -M $mode
+end
+fish_vi_key_bindings
