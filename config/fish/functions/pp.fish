510ff2753563f42615a91005ac419ccc9b397a5b
staticWagomU <to6wa17@gmail.com>
2024-04-28 21:39:32 +0900

    fish: ghqでとってきたレポジトリにすぐに移動する

---
+++ config/fish/functions/pp.fish
@@ -0,0 +1,16 @@
+function pp --description 'cd to a github project directory'
+  set -l DEVDIR ~/dev/github.com/
+  set -l GITHUBUSER (ls $DEVDIR -1 | peco)
+  
+  if test -n "$GITHUBUSER"
+    set -l PROJECT (ls $DEVDIR/$GITHUBUSER -1 | peco)
+    
+    if test -n "$PROJECT"
+      cd $DEVDIR/$GITHUBUSER/$PROJECT
+    else
+      echo "No project selected."
+    end
+  else
+    echo "No user selected."
+  end
+end
