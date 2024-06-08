b4fcd3ba6a929eb28cc1da6ecf9a33cc8d5d3db9
staticWagomU <to6wa17@gmail.com>
2024-06-08 15:32:27 +0900

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -44,7 +44,7 @@ fish_add_path $aqua_bin_dir
 
 # mise
 fish_add_path ~/.local/share/mise
-
+fish_add_path ~/.local/share/mise/shims
 # deno
 set -gx DENO_INSTALL "$HOME/.deno"
 fish_add_path "$DENO_INSTALL/bin"
