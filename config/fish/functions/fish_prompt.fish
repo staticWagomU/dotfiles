e09eeaf5116829bd86a3cb661a0c1527ec133d9a
staticWagomU <to6wa17@gmail.com>
2024-08-21 11:15:04 +0900

    fish: update prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -13,8 +13,8 @@ function fish_prompt --description 'Informative prompt'
     set -l statusb_color (set_color --bold $fish_color_status)
     set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    printf '%s %s%s@%s %s%s %s%s \n> ' (fish_git_prompt) (set_color brblue) \
-      $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+    printf '%s %s%s %s%s %s%s \n> ' (fish_git_prompt) (set_color brblue) \
+      $USER (set_color $fish_color_cwd) $PWD $pipestatus_string \
       (set_color normal) 
   end
 end
