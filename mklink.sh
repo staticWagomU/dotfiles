430919bfb5ef9cfdc148e70539e878e9282f47f0
staticWagomU <to6wa17@gmail.com>
2024-09-06 19:02:25 +0900

    mklink: update

--- mklink.sh
+++ mklink.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-rm -rf ~/.config/wezterm/wezterm.lua
-rm -rf ~/.wezterm.lua
-rm -rf ~/.config/fish/config.fish
-rm -rf ~/.config/fish/functions
-rm -rf ~/aqua.yaml
-rm -rf ~/.gitconfig
-ln -s ~/dotfiles/config/wezterm ~/.config/wezterm
-ln -s ~/dotfiles/config/wezterm/wezterm.lua ~/.wezterm.lua
-ln -s ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
-ln -s ~/dotfiles/config/fish/functions ~/.config/fish/functions
-ln -s ~/dotfiles/config/aqua.yaml ~/aqua.yaml
-ln -s ~/dotfiles/config/.tigrc ~/.tigrc
+ln -sf ~/dotfiles/config/wezterm ~/.config/wezterm
+ln -sf ~/dotfiles/config/wezterm/wezterm.lua ~/.wezterm.lua
+ln -sf ~/dotfiles/config/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/config/fish/completions ~/.config/fish/completions
+ln -sf ~/dotfiles/config/fish/functions ~/.config/fish/functions
+ln -sf ~/dotfiles/config/aqua.yaml ~/aqua.yaml
+ln -sf ~/dotfiles/config/.tigrc ~/.tigrc
 cp ~/dotfiles/config/.gitconfig_other ~/.gitconfig
