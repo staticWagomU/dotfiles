62d881dad2e15d9489d0e9031a8cb641c6aad80b
staticWagomU <to6wa17@gmail.com>
2024-11-21 21:11:00 +0900

    fish: 直でホームディレクトリを指定している馬鹿です...

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -136,7 +136,7 @@ fish_vi_key_bindings
 status --is-interactive; and rbenv init - fish | source
 
 # pnpm
-set -gx PNPM_HOME "/home/beige/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
