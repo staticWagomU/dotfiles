98ba117afc8be94c6ea5417418a0e1ff47a85884
staticWagomU <to6wa17@gmail.com>
2025-05-01 12:36:46 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -41,24 +41,24 @@
         "x86_64-linux"
       ];
 
-      perSystem =
-        { ... }:
-        {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs = {
-              actionlint.enable = true;
-              nixfmt.enable = true;
-              taplo.enable = true;
-              jsonfmt.enable = true;
-              yamlfmt.enable = true;
-              fish_indent.enable = true;
-              stylua.enable = true;
-              shfmt.enable = true;
-              prettier.enable = true;
-            };
-          };
-        };
+#       perSystem =
+#         { ... }:
+#         {
+#           treefmt = {
+#             projectRootFile = "flake.nix";
+#             programs = {
+#               actionlint.enable = true;
+#               nixfmt.enable = true;
+#               taplo.enable = true;
+#               jsonfmt.enable = true;
+#               yamlfmt.enable = true;
+#               fish_indent.enable = true;
+#               stylua.enable = true;
+#               shfmt.enable = true;
+#               prettier.enable = true;
+#             };
+#           };
+#         };
 
 #       flake = {
 #         darwinConfigurations = {
