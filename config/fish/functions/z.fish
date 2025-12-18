1bbf6b156f9d0f3cda8aa283b45e337dcab7d4c0
staticWagomU <to6wa17@gmail.com>
2025-12-19 05:12:16 +0900

    feat: ありがとう atusyさん

---
+++ config/fish/functions/z.fish
@@ -0,0 +1,24 @@
+# refs: https://blog.atusy.net/2025/05/09/zoxide-with-ghq/
+function __zoxide_list_missing
+  diff \
+    ( zoxide query --list | sort | psub ) \
+    ( ghq list -p | sort | psub ) \
+    | string match --regex --entire '^> ' \
+    | string replace -r '^> ' ''
+end
+
+function __zoxide_add_missing
+  set -l missing ( __zoxide_list_missing )
+  if test ( count $missing ) -gt 0
+    zoxide add $missing
+  end
+end
+
+function z --description 'zoxide wrapper'
+  __zoxide_add_missing
+  if test (count $argv) -eq 1
+    __zoxide_z $argv || true
+  else
+    __zoxide_zi $argv || true
+  end
+end
