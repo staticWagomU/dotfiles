a30c0245dae98a761c2ca2d9fc13a7757fd75be1
staticWagomU <to6wa17@gmail.com>
2026-01-07 16:39:51 +0900

    bunつかうぜ

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,6 +27,8 @@ fish_add_path ~/.npm-global/bin
 fish_add_path /nix/var/nix/profiles/default/bin
 fish_add_path ~/.nix-profile/bin
 
+fish_add_path ~/.bun/bin
+
 if test (uname -s) = "Darwin"
   set -l paths \
     /opt/homebrew/bin \
