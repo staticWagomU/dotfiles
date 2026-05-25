c6e4c1d45ab9dcb939fd29e39faf11e3687a71bc
staticWagomU <to6wa17@gmail.com>
2026-05-25 15:58:34 +0900

    feat(fish): ghp で所属 org のリポジトリも選択対象にする
    自分のリポジトリだけでなく所属 org のリポジトリも ghq get したいため、
    gh org list で得た各 owner を xargs -P で並列取得するよう変更。
    直列ループだと org 数に比例して遅くなるので並列化し、sort -u で重複排除。
    
    behavioral change

--- config/fish/functions/ghp.fish
+++ config/fish/functions/ghp.fish
@@ -1,3 +1,7 @@
 function ghp --description 'gh + fzf + ghq'
-  gh repo list --json url -q '.[].url' | fzf | ghq get
+  # 自分とorg一覧をownerとしてxargs -Pで並列にrepo取得（直列だとorg数に比例して遅い）
+  begin
+    gh api user -q .login
+    gh org list
+  end | xargs -P 8 -I@ gh repo list @ -L 1000 --no-archived --json url -q '.[].url' | sort -u | fzf | ghq get
 end
